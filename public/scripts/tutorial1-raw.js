7fe72679b8063c1b8508e6ac15693c0ab727a25b
Edvin Salo <best.fast@Edvins-Mac.local>
2016-03-30 23:47:28 -0700

    react comment tutorial save

---
+++ public/scripts/tutorial1-raw.js
@@ -0,0 +1,15 @@
+var CommentBox = React.createClass({
+	displayName: 'CommentBox',
+	render: function() {
+		return (
+			React.createElement('div', {className: "commentBox"},
+				"Hello, world! I am a CommentBox."
+			)
+		);
+	}
+});
+
+ReactDOM.render(
+	React.createElement(CommentBox, null),
+	document.getElementById('content')
+);
